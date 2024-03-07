fec9bb7a2f75cba0d83d292b11f0e2a4704284c1
luchoro90 <luchoplay867@gmail.com>
2024-03-06 21:01:55 -0300

    desafio

---
+++ src/dao/models/carts.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const schemaOptions = {
+    versionKey: false
+};
+
+const cartCollection = 'carts';
+
+const cartSchema = new mongoose.Schema({
+    products: {
+        type: [
+            {
+                product: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "products"
+                },
+                quantity: Number,
+                _id: false,
+            },
+        ],
+        default: [],
+    }
+}, schemaOptions);
+
+
+const cartModel = mongoose.model(cartCollection, cartSchema)
+
+module.exports = {
+    cartModel
+}
